76dc26c0feb6defa02ef46a6101911bb9849ca51
Haseeb Qureshi <haseeb@haseebq.com>
2015-06-17 17:01:53 -0700

    Add randomized name changes, fix formatting

--- app/assets/javascripts/views/options/fields_form.js
+++ app/assets/javascripts/views/options/fields_form.js
@@ -2,11 +2,35 @@ ABetterPresent.Views.FieldsForm = Backbone.CompositeView.extend({
   template: JST['options/fields_form'],
 
   render: function () {
-    this.$el.html(this.template({ card: this.model }));
+    this.$el.html(this.template({ card: this.model, name: this.randomName() }));
     this.$('#amount').maskMoney({
       prefix: "$",
       affixesStay: false,
     })
     return this;
+  },
+
+  randomName: function () {
+    return _.sample([
+      "Winston Churchill",
+      "Abraham Lincoln",
+      "Dr. Frankenstein",
+      "Mother Goose",
+      "Huckleberry Finn",
+      "Casper the Friendly Ghost",
+      "My cats, collectively",
+      "Rhesus of Thrace",
+      "To whom it may concern",
+      "Quetzacoatl",
+      "Harrison Ford",
+      "All of the Jackson Five",
+      "Magni, son of Thor",
+      "Galileo Galilei",
+      "Freddy Mercury",
+      "William Wallace",
+      "Jiminy Cricket",
+      "Bilbo Baggins",
+      "What's-his-face",
+    ]);
   },
 });
